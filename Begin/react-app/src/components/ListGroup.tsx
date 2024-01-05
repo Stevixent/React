d19e91c31d359372d340cd4192c7da55397775cc
Virtue Chiadika <chiadikavirtue@gmail.com>
2024-01-05 21:12:21 +0100

    item

--- Begin/react-app/src/components/ListGroup.tsx
+++ Begin/react-app/src/components/ListGroup.tsx
@@ -1,15 +1,14 @@
 import { Fragment } from "react";
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
     <Fragment>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">A fifth item</li>
-        <li className="list-group-item">A sixth item</li>
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
       </ul>
     </Fragment>
   );
