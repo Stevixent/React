c8db5e581b973959e00df33b162efd568f90262b
Virtue Chiadika <chiadikavirtue@gmail.com>
2024-01-05 21:02:35 +0100

    ok

--- Begin/react-app/src/components/ListGroup.tsx
+++ Begin/react-app/src/components/ListGroup.tsx
@@ -1,5 +1,12 @@
 function ListGroup() {
-    return <h1>List Group</h1>;
+    return <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">A fifth item</li>
+        <li className="list-group-item">A sixth item</li>
+    </ul>
 }
 
 export default ListGroup;
