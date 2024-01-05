e5c3255312f469873373c8ead5bda56e914bd7ff
Virtue Chiadika <chiadikavirtue@gmail.com>
2024-01-05 21:08:06 +0100

    ok

--- Begin/react-app/src/components/ListGroup.tsx
+++ Begin/react-app/src/components/ListGroup.tsx
@@ -1,13 +1,17 @@
+import { Fragment } from "react";
 function ListGroup() {
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">A fifth item</li>
-      <li className="list-group-item">A sixth item</li>
-    </ul>
+    <Fragment>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">A fifth item</li>
+        <li className="list-group-item">A sixth item</li>
+      </ul>
+    </Fragment>
   );
 }
 
