ba29e9de150d14c843e77bddaedcf26bb504bb15
Virtue Chiadika <chiadikavirtue@gmail.com>
2024-01-06 20:40:31 +0100

    ok

--- Begin/react-app/src/components/ListGroup.tsx
+++ Begin/react-app/src/components/ListGroup.tsx
@@ -5,6 +5,7 @@ function ListGroup() {
   return (
     <Fragment>
       <h1>List</h1>
+      {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
